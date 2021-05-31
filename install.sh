e6c997bea5f10d07a25ba4bded1e401b3f076027
ayesumit <darkeyesmit@gmail.com>
2021-05-31 21:20:09 +0530

    Soon Realeasing 😃

--- install.sh
+++ install.sh
@@ -4,20 +4,19 @@ sleep 2
 echo "Just Sit Back And Relax.."
 sleep 2
 
-chmod +x $HOME/Termux-Desk/*
-
 #installing All Required Materials.
 echo "Installing x11-repo"
 apt update && apt upgrade -y
 apt install x11-repo -y
 apt install git wget curl micro tar zip unzip -y
+
 echo "Installing Xfce4 And Its Dependencies
 Might Take A While..."
 apt install xfce4 xfce4-terminal xfce4-taskmanager xfce4-whiskermenu-plugin xfce4-clipman-plugin gtk3 gtk2 -y
 apt install xfce4* -y
 
 echo "Installing System Apps"
-apt install xarchiver neofetch htop leafpad lsd nnn bat cmatrix -y
+apt install xarchiver neofetch htop leafpad lsd nnn bat ncdu cmatrix -y
 
 #echo "Installing Netsurf Browser, But it Doesn't Support Js"
 #apt install netsurf -y
@@ -32,17 +31,21 @@ echo "Installing Vnc Client.."
 apt install tigervnc -y
 
 echo ".............................."
-echo "Applications Installed !"
+echo ".  Applications Installed !  ."
 echo ".............................."
 
+sleep 2
+
 echo "Creating Folders"
-#Create Folder For Symlinks.
 mkdir $HOME/Desktop
 mkdir $HOME/Downloads
 mkdir $HOME/Walpapers 
 mkdir $HOME/Documents
 mkdir $HOME/DCIM
 mkdir $HOME/Android
+mkdir $HOME/.icons
+mkdir $HOME/.themes
+mkdir $HOME/.vnc
 
 echo "Creating Symlinks"
 # Create Symlinks To Android Storage.
@@ -50,7 +53,7 @@ ln -s $HOME/storage/dcim/* $HOME/DCIM
 ln -s $HOME/storage/shared/* $HOME/Android
 
 echo "Setting Up Vnc Server"
-cp -rf $HOME/Termux-Desk/Dotfiles/.vnc $HOME
+cp -rf $HOME/Termux-Desk/Dotfiles/xstartup $HOME/.vnc
 cp -rf $HOME/Termux-Desk/Dotfiles/startdesk $PREFIX/bin
 cp -rf $HOME/Termux-Desk/Dotfiles/stopdesk $PREFIX/bin
 chmod +x $PREFIX/bin/startdesk
@@ -58,27 +61,22 @@ chmod +x $PREFIX/bin/stopdesk
 chmod +x $HOME/.vnc/xstartup
 
 echo "Setting Up Dotfiles..."
-#Setting Up Dotfiles
 cp -rf $HOME/Termux-Desk/Dotfiles/.config $HOME
 cp -rf $HOME/Termux-Desk/Dotfiles/.vimrc $HOME
 cp -rf $HOME/Termux-Desk/Dotfiles $HOME
 
-echo "Setting Up Desktop Environment Configs."
-#Setting Up Themes, Icons, Walpaper
-mkdir $HOME/.icons
-mkdir $HOME/.themes
-#Cloning My Walpapers Repository
+echo "Setting Up Themes, Icons, Walpaper"
 git clone https://github.com/ayesumit/Walpapers ~/Walpapers
 
-#Cloning Themes from Source Repos
-cp -rf $HOME/Termux-Desk/Dotfiles/themes.tar.gz $HOME/.themes
-cd ~/.themes && tar xf themes.tar.gz
+cp -rf $HOME/Termux-Desk/Dotfiles/themes/theme.tar.gz $HOME/.themes
+cd ~/.themes && tar xf theme.tar.gz
 
-#Cloning Icons From Source Repos
-cp -rf $HOME/Termux-Desk/Dotfiles/icons.tar.gz $HOME/.icons
-cd ~/.icons && tar xf icons.tar.gz
+cp -rf $HOME/Termux-Desk/Dotfiles/themes/icon.tar.gz $HOME/.icons
+cd ~/.icons && tar xf icon.tar.gz
+
+cp -rf $HOME/Termux-Desk/Dotfiles/themes/cursor.tar.gz $HOME/.icons
+cd ~/.icons && tar xf cursor.tar.gz
+
+bat $HOME/Termux-Desk/Dotfiles/experimental/greet.txt
 cd $HOME
-
-bat $HOME/Termux-Desk/Dotfiles/greet.txt
-
 
--- propedit.sh
+++ propedit.sh
@@ -1,11 +1,11 @@
 echo "UnMessing The Messing Things, Relax"
 termux-setup-storage
-
 apt update && apt upgrade -y
 
-cp -rf $HOME/Termux-Desk/Dotfiles/font.ttf $HOME/.termux
-cp -rf $HOME/Termux-Desk/Dotfiles/colors.properties $HOME/.termux
-cp -rf $HOME/Termux-Desk/Dotfiles/termux.properties $HOME/.termux
+mkdir .termux
+cp -rf $HOME/Termux-Desk/Dotfiles/termux/font.ttf $HOME/.termux
+cp -rf $HOME/Termux-Desk/Dotfiles/termux/colors.properties $HOME/.termux
+cp -rf $HOME/Termux-Desk/Dotfiles/termux/termux.properties $HOME/.termux
 
 termux-reload-settings
 exit
